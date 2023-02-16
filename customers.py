c36c4b12f74ea459d7ade50de87ac64a6528b542
Samantha Dunlop <sammyd112@yahoo.com>
2023-02-16 10:01:34 -0500

    Inital Commit-- Not completed

---
+++ customers.py
@@ -0,0 +1,17 @@
+"""Customers at Hackbright."""
+
+
+class Customer:
+    """Ubermelon customer."""
+    
+    # TODO: need to implement this
+def __init__(self, name):
+    self.name=name
+    
+
+
+
+def log_in(self, email, password):
+    self.__init__
+    self.email = email
+    self.password = password
